d5ac125dc856b6c5c5abfc2610b6ef95e13dffa6
Lonny Gomes <code@lonnygomes.com>
2020-04-09 22:34:19 -0400

    feat(dm): expose the images and names from the data

--- src/app/services/data-manager.service.ts
+++ src/app/services/data-manager.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import data from './data.json';
 
+const BASE_IMG_PATH = 'assets/images';
 @Injectable({
     providedIn: 'root',
 })
 export class DataManagerService {
     constructor() {}
+
+    get names(): string[] {
+        return data.names;
+    }
+
+    get images(): string[] {
+        return data.images.map((img) => `${BASE_IMG_PATH}/${img}`);
+    }
 }
