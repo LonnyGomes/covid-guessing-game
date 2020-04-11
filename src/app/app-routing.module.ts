c93634de7786ff86d3ac67ae3f504dc1fd9ddadb
Lonny Gomes <code@lonnygomes.com>
2020-04-11 12:17:01 -0400

    wip: add /splash route
    refs #4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ResultsComponent } from './results/results.component';
+import { SplashComponent } from './splash/splash.component';
 
 const routes: Routes = [
     { path: '', component: AppComponent },
     { path: 'results', component: ResultsComponent },
+    { path: 'splash', component: SplashComponent },
     { path: 'results/:guessData', component: ResultsComponent },
     { path: '', redirectTo: '', pathMatch: 'full' },
 ];
