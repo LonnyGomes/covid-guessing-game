f28e23550c1958be24f911e091c42b5bca15f8db
Lonny Gomes <code@lonnygomes.com>
2020-04-11 04:30:35 -0400

    chore: add material icons module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,26 @@ import { AppComponent } from './app.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NamesDialogComponent } from './names-dialog/names-dialog.component';
 import { EntryComponent } from './entry/entry.component';
 import { ResultsComponent } from './results/results.component';
 
 @NgModule({
-    declarations: [AppComponent, NamesDialogComponent, EntryComponent, ResultsComponent],
+    declarations: [
+        AppComponent,
+        NamesDialogComponent,
+        EntryComponent,
+        ResultsComponent,
+    ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         MatDialogModule,
         MatButtonModule,
         MatCardModule,
+        MatIconModule,
         BrowserAnimationsModule,
     ],
     providers: [],
